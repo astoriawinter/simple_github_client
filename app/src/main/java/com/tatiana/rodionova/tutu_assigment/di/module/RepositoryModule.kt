4163b2f4f033c064266671882b43ce71c89385dd
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-07-05 14:58:37 +0300

    app: add detailed screen dependencies

--- app/src/main/java/com/tatiana/rodionova/tutu_assigment/di/module/RepositoryModule.kt
+++ app/src/main/java/com/tatiana/rodionova/tutu_assigment/di/module/RepositoryModule.kt
@@ -1,15 +1,22 @@
 package com.tatiana.rodionova.tutu_assigment.di.module
 
 import com.tatiana.rodionova.data.api.GithubService
-import com.tatiana.rodionova.data.repository.GithubRepositoryImplementation
-import com.tatiana.rodionova.domain.repository.GithubRepository
+import com.tatiana.rodionova.data.repository.GithubRepositoryDetailedImplementation
+import com.tatiana.rodionova.data.repository.GithubRepositoryListImplementation
+import com.tatiana.rodionova.domain.repository.GithubDetailedRepository
+import com.tatiana.rodionova.domain.repository.GithubListRepository
 import dagger.Module
 import dagger.Provides
 
 @Module
 class RepositoryModule {
     @Provides
-    fun providesGithubRepository(githubService: GithubService, key: String): GithubRepository {
-        return GithubRepositoryImplementation(key, githubService)
+    fun providesGithubListRepository(githubService: GithubService, key: String): GithubListRepository {
+        return GithubRepositoryListImplementation(key, githubService)
+    }
+
+    @Provides
+    fun providesGithubDetailedRepository(githubService: GithubService): GithubDetailedRepository {
+        return GithubRepositoryDetailedImplementation(githubService)
     }
 }
