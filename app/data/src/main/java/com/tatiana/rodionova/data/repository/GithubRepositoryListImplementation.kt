7d147803ff622ebd037a835fc849d2a1a8dc38e9
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-07-05 15:06:40 +0300

    data: update GithubRepositoryListImplementation formatting

--- app/data/src/main/java/com/tatiana/rodionova/data/repository/GithubRepositoryListImplementation.kt
+++ app/data/src/main/java/com/tatiana/rodionova/data/repository/GithubRepositoryListImplementation.kt
@@ -3,19 +3,19 @@ package com.tatiana.rodionova.data.repository
 import com.tatiana.rodionova.data.api.GithubService
 import com.tatiana.rodionova.data.mapper.toDomain
 import com.tatiana.rodionova.data.model.GithubItem
-import com.tatiana.rodionova.domain.model.GithubRepositoryDomainItem
-import com.tatiana.rodionova.domain.repository.GithubRepository
+import com.tatiana.rodionova.domain.model.GithubRepositoryListDomainItem
+import com.tatiana.rodionova.domain.repository.GithubListRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class GithubRepositoryImplementation(
+class GithubRepositoryListImplementation(
     private val key: String,
     private val githubService: GithubService
-): GithubRepository {
+) : GithubListRepository {
 
-    override suspend fun getGithubAndroidRepositories(): Flow<List<GithubRepositoryDomainItem>> =
+    override suspend fun getGithubAndroidRepositories(): Flow<List<GithubRepositoryListDomainItem>> =
         flow {
             emit(
                 githubService
