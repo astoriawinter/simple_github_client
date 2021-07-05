c1f12f1ba2c9091d628a54e581b38f3c5a3ae841
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-07-05 15:03:43 +0300

    data: add getTreeStructure method to GithubService

--- app/data/src/main/java/com/tatiana/rodionova/data/api/GithubService.kt
+++ app/data/src/main/java/com/tatiana/rodionova/data/api/GithubService.kt
@@ -1,11 +1,19 @@
 package com.tatiana.rodionova.data.api
 
 import com.tatiana.rodionova.data.model.GithubRepositoryModel
+import com.tatiana.rodionova.data.model.TreeStructureModel
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
 
     @GET("search/repositories")
     suspend fun getRepositories(@Query("q") key: String): GithubRepositoryModel
+
+    @GET("repos/{name}/{repo}/git/trees/master")
+    suspend fun getTreeStructure(
+        @Path("name") name: String,
+        @Path("repo") repo: String
+    ): TreeStructureModel
 }
