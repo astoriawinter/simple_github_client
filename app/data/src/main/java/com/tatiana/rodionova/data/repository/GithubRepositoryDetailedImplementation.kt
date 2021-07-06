385e5592e7801a4fd863670c5f280dfc5d4a6a22
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-07-07 01:04:43 +0300

    data: repository: add implementations for list & detailed screens

--- app/data/src/main/java/com/tatiana/rodionova/data/repository/GithubRepositoryDetailedImplementation.kt
+++ app/data/src/main/java/com/tatiana/rodionova/data/repository/GithubRepositoryDetailedImplementation.kt
@@ -1,29 +1,70 @@
 package com.tatiana.rodionova.data.repository
 
 import com.tatiana.rodionova.data.api.GithubService
+import com.tatiana.rodionova.data.api.helper.NetworkHelper
+import com.tatiana.rodionova.data.database.dao.TreeItemDao
 import com.tatiana.rodionova.data.mapper.toDomain
-import com.tatiana.rodionova.data.model.Tree
+import com.tatiana.rodionova.data.mapper.toEntity
+import com.tatiana.rodionova.data.model.dto.Tree
+import com.tatiana.rodionova.data.model.entity.TreeItemEntity
 import com.tatiana.rodionova.domain.model.GithubRepositoryTreeDomainItem
+import com.tatiana.rodionova.domain.model.getFullRepositoryName
 import com.tatiana.rodionova.domain.repository.GithubDetailedRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 
 class GithubRepositoryDetailedImplementation(
-    private val githubService: GithubService
+    private val githubService: GithubService,
+    private val githubTreeItemDao: TreeItemDao,
+    private val networkHelper: NetworkHelper
 ) : GithubDetailedRepository {
 
-    override suspend fun getGithubRepositoryTrees(
+    override fun getGithubRepositoryTrees(
+        name: String,
+        repo: String
+    ): Flow<List<GithubRepositoryTreeDomainItem>> =
+        if (networkHelper.isNetworkConnected.value) {
+            getTreeListFromApiAndUpdateDBAsFlow(name, repo)
+        } else getTreeListFromDBAndUpdateFromApi(name, repo)
+
+    private suspend fun getTreeListFromApi(name: String, repo: String): List<Tree> =
+        githubService
+            .getTreeStructure(name, repo)
+            .tree
+
+    private fun getTreeListFromApiAndUpdateDB(
         name: String,
         repo: String
     ): Flow<List<GithubRepositoryTreeDomainItem>> =
         flow {
-            emit(
-                githubService
-                    .getTreeStructure(name, repo)
-                    .tree
-                    .map(Tree::toDomain)
+            val treeList = getTreeListFromApi(name, repo)
+            emit(treeList.map(Tree::toDomain))
+
+            githubTreeItemDao.saveTreeList(
+                treeList.map { tree ->
+                    tree.toEntity(getFullRepositoryName(name, repo))
+                }
             )
-        }.flowOn(Dispatchers.Default)
+        }
+
+    private fun getTreeListFromApiAndUpdateDBAsFlow(
+        name: String,
+        repo: String
+    ): Flow<List<GithubRepositoryTreeDomainItem>> =
+        getTreeListFromApiAndUpdateDB(name, repo).flowOn(Dispatchers.IO)
+
+    private fun getTreeListFromDBAndUpdateFromApi(
+        name: String,
+        repo: String
+    ): Flow<List<GithubRepositoryTreeDomainItem>> =
+        networkHelper.isNetworkConnected
+            .flatMapLatest { isConnected ->
+                if (isConnected) {
+                    getTreeListFromApiAndUpdateDB(name, repo)
+                } else {
+                    githubTreeItemDao.getTreeListByNameAndRepo(name).map { treeList ->
+                        treeList.map(TreeItemEntity::toDomain)
+                    }
+                }
+            }.flowOn(Dispatchers.IO)
 }
--- app/data/src/main/java/com/tatiana/rodionova/data/repository/GithubRepositoryListImplementation.kt
+++ app/data/src/main/java/com/tatiana/rodionova/data/repository/GithubRepositoryListImplementation.kt
@@ -1,27 +1,58 @@
 package com.tatiana.rodionova.data.repository
 
 import com.tatiana.rodionova.data.api.GithubService
+import com.tatiana.rodionova.data.api.helper.NetworkHelper
+import com.tatiana.rodionova.data.database.dao.GithubItemDao
 import com.tatiana.rodionova.data.mapper.toDomain
-import com.tatiana.rodionova.data.model.GithubItem
+import com.tatiana.rodionova.data.mapper.toEntity
+import com.tatiana.rodionova.data.model.dto.GithubItem
+import com.tatiana.rodionova.data.model.entity.GithubItemEntity
 import com.tatiana.rodionova.domain.model.GithubRepositoryListDomainItem
 import com.tatiana.rodionova.domain.repository.GithubListRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 
 class GithubRepositoryListImplementation(
     private val key: String,
-    private val githubService: GithubService
+    private val sortBy: String,
+    private val githubService: GithubService,
+    private val githubItemDao: GithubItemDao,
+    private val networkHelper: NetworkHelper
 ) : GithubListRepository {
 
-    override suspend fun getGithubAndroidRepositories(): Flow<List<GithubRepositoryListDomainItem>> =
+    override fun getGithubAndroidRepositories(): Flow<List<GithubRepositoryListDomainItem>> =
+        if (networkHelper.isNetworkConnected.value) {
+            getGithubListFromApiAndUpdateDBAsFlow()
+        } else getGithubListFromDBOrUpdateFromApi()
+
+    private suspend fun getGithubListFromApi() =
+        githubService
+            .getRepositories(key, sortBy)
+            .items
+
+    private fun getGithubListFromApiAndUpdateDB(): Flow<List<GithubRepositoryListDomainItem>> =
         flow {
-            emit(
-                githubService
-                    .getRepositories(key)
-                    .items
-                    .map(GithubItem::toDomain)
+            val githubList = getGithubListFromApi()
+
+            emit(githubList.map(GithubItem::toDomain))
+
+            githubItemDao.saveGithubItemList(
+                githubList.map(GithubItem::toEntity)
             )
-        }.flowOn(Dispatchers.Default)
+        }
+
+    private fun getGithubListFromApiAndUpdateDBAsFlow(): Flow<List<GithubRepositoryListDomainItem>> =
+        getGithubListFromApiAndUpdateDB().flowOn(Dispatchers.IO)
+
+    private fun getGithubListFromDBOrUpdateFromApi(): Flow<List<GithubRepositoryListDomainItem>> =
+        networkHelper.isNetworkConnected
+            .flatMapLatest { isConnected ->
+                if (isConnected) {
+                    getGithubListFromApiAndUpdateDB()
+                } else {
+                    githubItemDao.getGithubItemList().map { treeList ->
+                        treeList.map(GithubItemEntity::toDomain)
+                    }
+                }
+            }.flowOn(Dispatchers.IO)
 }
