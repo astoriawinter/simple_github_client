d61d189dae8daece31c1f7eb5750595fabf91083
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-07-07 00:48:22 +0300

    data: api: add sort key to GithubService

--- app/data/src/main/java/com/tatiana/rodionova/data/api/GithubService.kt
+++ app/data/src/main/java/com/tatiana/rodionova/data/api/GithubService.kt
@@ -1,7 +1,7 @@
 package com.tatiana.rodionova.data.api
 
-import com.tatiana.rodionova.data.model.GithubRepositoryModel
-import com.tatiana.rodionova.data.model.TreeStructureModel
+import com.tatiana.rodionova.data.model.dto.GithubRepositoryModel
+import com.tatiana.rodionova.data.model.dto.TreeStructureModel
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -9,7 +9,10 @@ import retrofit2.http.Query
 interface GithubService {
 
     @GET("search/repositories")
-    suspend fun getRepositories(@Query("q") key: String): GithubRepositoryModel
+    suspend fun getRepositories(
+        @Query("q") key: String,
+        @Query("sort") sortBy: String
+    ): GithubRepositoryModel
 
     @GET("repos/{name}/{repo}/git/trees/master")
     suspend fun getTreeStructure(
